7127cefc18333d2da9f5384df1cd84cc596435a8
subhamstark <subham@sap.com>
2020-10-05 11:02:06 +0530

    UC3 Done

--- src/com/workshop/tictactoeGame/TicTacToeGame.java
+++ src/com/workshop/tictactoeGame/TicTacToeGame.java
@@ -6,11 +6,14 @@ import java.util.Scanner;
 public class TicTacToeGame {
 	
 	//Class Variables
+	private static final char ROUND='O';
+	private static final char CROSS='X';
+	private static final char BLANK=' ';
 	private char[] board ;
 	private static int boardSize=10;
 	private char playerChar;
 	private char opponentChar;
-	
+	private final Scanner scannerObj = new Scanner(System.in);
 	
 	
 	//Class Functions
@@ -18,29 +21,28 @@ public class TicTacToeGame {
 	{
 		//instantiate char board with board_size
 		board = new char[boardSize];
-		Arrays.fill(board, ' ');
+		Arrays.fill(board, BLANK);
 	}
 	private void player_choice_XO() {
-		Scanner sc=new Scanner(System.in);
 		System.out.println("Please Choose X or O");
 		
 		//Take Input from user for Token (i.e X or O)
-		playerChar=sc.next().charAt(0); 
-		opponentChar = (playerChar == 'X') ? 'O' : 'X';
-		sc.close();
+		playerChar=scannerObj.next().toUpperCase().charAt(0); 
+		opponentChar = (playerChar == CROSS) ? ROUND : CROSS;
 	}
 	
 	private void showPlayer() {
+		//Showing the Token for Player and Opponent
 		System.out.println("Player Token " + playerChar + " Opponent Token " + opponentChar);
 	}
 	
 	private void showBoard() {
+		//Printing the Current State of the Game Board
 		System.out.println("\n**** Board ****");
-	    System.out.println("  " + board[1] + " | " + board[2] + " | " + board[3]);  
-	    System.out.println("-------------");
-	    System.out.println("  " + board[4] + " | " + board[5] + " | " + board[6]);  
-	    System.out.println("-------------");
-	    System.out.println("  " + board[7] + " | " + board[8] + " | " + board[9]); 
+		for(int i=1;i<boardSize;i=i+3) {
+			System.out.println("  " + board[i] + " | " + board[i+1] + " | " + board[i+2]); 
+		    System.out.println("-------------");
+		}
 	}
 	public static void main(String args[]) {
 		System.out.println("Game");
