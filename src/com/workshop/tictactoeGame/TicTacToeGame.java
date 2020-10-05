db7e41394d4d1971c3fc306a0ce3ec9b4d0109e3
subhamstark <subham@sap.com>
2020-10-05 16:44:04 +0530

    UC8 Done

--- src/com/workshop/tictactoeGame/TicTacToeGame.java
+++ src/com/workshop/tictactoeGame/TicTacToeGame.java
@@ -64,26 +64,45 @@ public class TicTacToeGame {
 
 	// Taking Choice of User to get the Current Position in Board
 	private int choosePosition() {
-		System.out.println("Choose any Number Between 1 and 9 ");
+		if(currentPlayer==COMPUTER)
+			return randomGeneratedPosition();
+		System.out.println(currentPlayer + " Choose any Number Between 1 and 9 ");
 		int currentPos = scannerObj.nextInt();
 		while ((currentPos >= boardSize || currentPos < 1) && !getStatus(currentPos)) {
-			System.out.println("Choose any Number Between 1 and 9 ");
+			System.out.println(currentPlayer + " Choose any Number Between 1 and 9 ");
 			currentPos = scannerObj.nextInt();
 		}
 		return currentPos;
 	}
 
+	//Random generated Position for Board
+	private int randomGeneratedPosition() {
+		int randomNum = (int) (Math.random() * 20 % 9);
+		randomNum++;
+		
+		while(!getStatus(randomNum)) {
+			randomNum = (int) (Math.random() * 20 % 9);
+			randomNum++;
+		}
+		return randomNum;
+	}
+
 	private void playerCurrentPosSetter(int cur) {
 		this.playerCurrentPosition = cur;
 	}
-	
-	//Players move in Board
+
+	// Players move in Board
 	private void playMove() {
-		board[playerCurrentPosition] = playerChar;
+		if (currentPlayer == USER)
+			board[playerCurrentPosition] = playerChar;
+		else
+			board[playerCurrentPosition] = opponentChar;
 		showBoard();
+
+		currentPlayer = (currentPlayer == USER) ? COMPUTER : USER;
 	}
-	
-	//Player Choose its Choice in TOSS
+
+	// Player Choose its Choice in TOSS
 	private void playerTossChoice() {
 		System.out.println("Enter Your Choice Press \n 1.Head \n 2.Tail ");
 		int choice = scannerObj.nextInt();
@@ -92,8 +111,8 @@ public class TicTacToeGame {
 		else
 			playerTossChoice = TAIL;
 	}
-	
-	//TOSS JUDGEMENT
+
+	// TOSS JUDGEMENT
 	private void tossWin() {
 		int randomNum = (int) (Math.random() * 20 % 2);
 		String tossResult;
@@ -111,50 +130,65 @@ public class TicTacToeGame {
 			currentPlayer = COMPUTER;
 		}
 	}
-	
+
+	//Board Winning Condition 
 	private boolean winningCondition() {
 		char checkChar;
-		if(currentPlayer == USER)
+		if (currentPlayer == USER)
 			checkChar = playerChar;
 		else
 			checkChar = opponentChar;
-		
-		//horizontal check
-		for(int i=1;i<boardSize;i+=3) {
-			if(board[i]==checkChar && board[i+1]==checkChar && board[i+2]==checkChar)
+
+		// horizontal check
+		for (int i = 1; i < boardSize; i += 3) {
+			if (board[i] == checkChar && board[i + 1] == checkChar && board[i + 2] == checkChar)
 				return true;
 		}
-		
-		//vertical check
-		for(int i=1;i<=3;i++) {
-			if(board[i]==checkChar && board[i+3]==checkChar && board[i+6]==checkChar)
+
+		// vertical check
+		for (int i = 1; i <= 3; i++) {
+			if (board[i] == checkChar && board[i + 3] == checkChar && board[i + 6] == checkChar)
 				return true;
 		}
-		
-		if((board[1]==board[5]) && (board[5]==board[9]) && (board[9]==checkChar))
+
+		if ((board[1] == board[5]) && (board[5] == board[9]) && (board[9] == checkChar))
 			return true;
-		
-		if((board[3]==board[5]) && (board[5]==board[7]) && (board[7]==checkChar))
+
+		if ((board[3] == board[5]) && (board[5] == board[7]) && (board[7] == checkChar))
 			return true;
-		
-		return false;	
+
+		return false;
+	}
+
+	//Game Starts HERE
+	private void start() {
+		board();
+		playerChoiceChar();
+		showPlayer();
+		playerTossChoice();
+		tossWin();
+		showBoard();
+		while (!isBoardFill()) {
+			playerCurrentPosSetter(choosePosition());
+			playMove();
+			if (winningCondition()) {
+				System.out.println(currentPlayer + " Wins the game \n GAME OVER");
+				break;
+			}
+		}
 	}
 	
+	//Checking Board is FULL or NOT
+	private boolean isBoardFill() {
+		for (int i = 1; i < boardSize; i++)
+			if (board[i] == BLANK)
+				return false;
+		return true;
+	}
+
 	public static void main(String args[]) {
 		System.out.println("Game");
 		TicTacToeGame game = new TicTacToeGame();
-		game.board();
-
-		game.playerChoiceChar();
-		game.showPlayer();
-		game.playerTossChoice();
-		game.tossWin();
-		game.showBoard();
-		
-		
-		game.playerCurrentPosSetter(game.choosePosition());
-		game.playMove();
-		System.out.println(game.winningCondition());
+		game.start();
 	}
-
 }
