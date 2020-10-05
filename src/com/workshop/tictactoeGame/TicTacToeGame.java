85705e6ecf1537f7deff39f420bc3f43e325a172
subhamstark <subham@sap.com>
2020-10-05 10:26:29 +0530

    UC3 Done

--- src/com/workshop/tictactoeGame/TicTacToeGame.java
+++ src/com/workshop/tictactoeGame/TicTacToeGame.java
@@ -7,9 +7,9 @@ public class TicTacToeGame {
 	
 	//Class Variables
 	private char[] board ;
-	private static int board_size=10;
-	private String player_char;
-	private String opponent_char;
+	private static int boardSize=10;
+	private char playerChar;
+	private char opponentChar;
 	
 	
 	
@@ -17,7 +17,7 @@ public class TicTacToeGame {
 	private void board()
 	{
 		//instantiate char board with board_size
-		board = new char[board_size];
+		board = new char[boardSize];
 		Arrays.fill(board, ' ');
 	}
 	private void player_choice_XO() {
@@ -25,15 +25,22 @@ public class TicTacToeGame {
 		System.out.println("Please Choose X or O");
 		
 		//Take Input from user for Token (i.e X or O)
-		player_char=sc.next();
-		if(player_char=="X")
-			opponent_char="O";
-		else
-			opponent_char="X";
+		playerChar=sc.next().charAt(0); 
+		opponentChar = (playerChar == 'X') ? 'O' : 'X';
+		sc.close();
 	}
 	
 	private void showPlayer() {
-		System.out.println("Player Token " + player_char + " Opponent Token " + opponent_char);
+		System.out.println("Player Token " + playerChar + " Opponent Token " + opponentChar);
+	}
+	
+	private void showBoard() {
+		System.out.println("\n**** Board ****");
+	    System.out.println("  " + board[1] + " | " + board[2] + " | " + board[3]);  
+	    System.out.println("-------------");
+	    System.out.println("  " + board[4] + " | " + board[5] + " | " + board[6]);  
+	    System.out.println("-------------");
+	    System.out.println("  " + board[7] + " | " + board[8] + " | " + board[9]); 
 	}
 	public static void main(String args[]) {
 		System.out.println("Game");
@@ -42,7 +49,9 @@ public class TicTacToeGame {
 		
 		game1.player_choice_XO();
 		game1.showPlayer();
+		game1.showBoard();
 	}
+	
 	
 	
 
