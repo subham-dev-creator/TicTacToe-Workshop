63f1a0497e9979911e9d044cbf02ad215a6a2afa
subhamstark <subham@sap.com>
2020-10-05 09:12:29 +0530

    UC 1 Done

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -1,4 +1,15 @@
+import java.util.Arrays;
 
 public class TicTacToeGame {
+	private char[] board ;
+	private void board()
+	{
+		board = new char[10];
+		Arrays.fill(board, ' ');
+	}
+	public static void main(String args[]) {
+		TicTacToeGame game1 = new TicTacToeGame();
+		game1.board();
+	}
 
 }
